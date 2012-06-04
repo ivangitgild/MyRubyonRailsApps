fc494fe57d786bc1f07083f5e9e9f6db57814126
ivangitgild <gonzales.ivan91@gmail.com>
2012-06-03 21:33:20 -0700

    update with password

--- myapps/app/models/adminuser.rb
+++ myapps/app/models/adminuser.rb
@@ -1,2 +1,24 @@
+require "digest"
+
 class Adminuser < ActiveRecord::Base
+	validates_presence_of :password
+	# Setup the salt value and hash the password before we save everything to the
+  # database.
+  def before_save
+    if (self.salt == nil)
+      self.salt = random_numbers(5)
+      self.password = Digest::MD5.hexdigest(self.salt + self.password)
+    end
+  end
+ 
+  private
+ 
+  # A sequence of random numbers with no skewing of range in any particular
+  # direction and leading zeros preserved.
+  def random_numbers(len)
+    numbers = ("0".."9").to_a
+    newrand = ""
+    1.upto(len) { |i| newrand << numbers[rand(numbers.size - 1)] }
+    return newrand
+  end
 end
